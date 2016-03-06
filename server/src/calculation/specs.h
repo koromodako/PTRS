58bdb6c656327839572f3a6c79e8f61a30f65324
Jonathan Guichard <jonathan38000@gmail.com>
2016-03-06 18:09:10 +0100

    Modification entrées/sorties du plugin bruteforce

--- server/src/calculation/specs.h
+++ server/src/calculation/specs.h
@@ -6,9 +6,10 @@
 #define CS_JSON_KEY_FRAG_ID     "fragment_id"
 #define CS_JSON_KEY_CALC_RESULT "result"
 
-#define CS_OP_SPLIT "-split"
-#define CS_OP_JOIN  "-join"
-#define CS_OP_CALC  "-calc"
+#define CS_OP_SPLIT "split"
+#define CS_OP_JOIN  "join"
+#define CS_OP_CALC  "calc"
+#define CS_EOF "EOF"
 
 #define CS_FRAGMENT_SEP ';'
 
