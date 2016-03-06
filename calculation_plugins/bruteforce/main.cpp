1c28aa1d2c6735ec7165a8411c016de34aa121ef
Jonathan Guichard <jonathan38000@gmail.com>
2016-03-06 18:09:10 +0100

    Modification entrées/sorties du plugin bruteforce

--- calculation_plugins/bruteforce/main.cpp
+++ calculation_plugins/bruteforce/main.cpp
@@ -7,52 +7,51 @@
 #include <QUrl>
 #include <QString>
 #include <QStringList>
+#include <QTextStream>
+#include <stdio.h>
 
 void success(std::string response);
 void fail(std::string msg);
 
 int main(int argc, char *argv[])
 {
-    // récupération des arguments en sautant le nom du programme
-    QStringList args;
-    for(int i = 1; i < argc; ++i) { args.append(QString(argv[i])); }
-    // traitement principal
-    if(args.count() != 2)
-    {   fail(QString("Expecting exactly 2 arguments : (%1|%2|%3) <json_url_encoded>").arg(CS_OP_SPLIT, CS_OP_CALC, CS_OP_JOIN).toStdString());
-    }
-    else
-    {   QString action = args[0];
-        QString json = QUrl::fromPercentEncoding(args[1].toUtf8());
-        if(action == CS_OP_JOIN)
-        {   Joiner joiner;
-            if(joiner.join(json))
-            {   success(QString(QUrl::toPercentEncoding(joiner.result())).toStdString());
-            }
-            else
-            {   fail(joiner.error().toStdString());
-            }
-        }
-        else if(action == CS_OP_SPLIT)
-        {   Splitter splitter;
-            if(splitter.split(json))
-            {   success(QString(QUrl::toPercentEncoding(splitter.result())).toStdString());
-            }
-            else
-            {   fail(splitter.error().toStdString());
-            }
-        }
-        else if(action == CS_OP_CALC)
-        {   Computer computer;
-            if(computer.compute(json))
-            {   success(QString(QUrl::toPercentEncoding(computer.result())).toStdString());
-            }
-            else
-            {   fail(computer.error().toStdString());
-            }
+    // récupération des paramètres de calcul
+    QTextStream qtin(stdin);
+    QString action;
+    QString json;
+    qtin >> action;
+    for(QString line; line != CS_EOF; line = qtin.readLine()) json += line;
+
+    // traitement principal    
+    if(action == CS_OP_JOIN)
+    {   Joiner joiner;
+        if(joiner.join(json))
+        {   success(QString(joiner.result()).toStdString());
         }
         else
-        {   fail("Unknown operation.");
+        {   fail(joiner.error().toStdString());
         }
+    }
+    else if(action == CS_OP_SPLIT)
+    {   Splitter splitter;
+        if(splitter.split(json))
+        {   success(QString(splitter.result()).toStdString());
+        }
+        else
+        {   fail(splitter.error().toStdString());
+        }
+    }
+    else if(action == CS_OP_CALC)
+    {   Computer computer;
+        if(computer.compute(json))
+        {   success(QString(computer.result()).toStdString());
+        }
+        else
+        {   fail(computer.error().toStdString());
+        }
+    }
+    else
+    {   fail("Unknown operation.");
     }
     // we should never reach this point
     exit(EXIT_FAILURE);
